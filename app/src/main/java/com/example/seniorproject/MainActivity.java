77e86622a7219c7e1bca4ccebe87278f426bf717
asmaa <asmaaelbanna21@gmail.com>
2020-08-07 13:52:46 +0200

    initial commit

---
+++ app/src/main/java/com/example/seniorproject/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.seniorproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pagetwo);
+        fragmentManager=getSupportFragmentManager();
+    }
+
+
+
+
+    public void mainClick(View view) {
+        fragmentManager.beginTransaction().replace(R.id.place_holder,new MainFragment()).commit();
+
+    }
+
+    public void offerClick(View view) {
+        fragmentManager.beginTransaction().replace(R.id.place_holder,new OfferFragment()).commit();
+    }
+
+    public void timeClick(View view) {
+        fragmentManager.beginTransaction().replace(R.id.place_holder,new TimeFragment()).commit();
+    }
+
+    public void likeClick(View view) {
+        fragmentManager.beginTransaction().replace(R.id.place_holder,new LikeFragment()).commit();
+    }
+
+    public void moreClick(View view) {
+        fragmentManager.beginTransaction().replace(R.id.place_holder,new MoreFragment()).commit();
+    }
+}
---
+++ app/src/main/java/com/example/seniorproject/MainFragment.java
@@ -0,0 +1,74 @@
+package com.example.seniorproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class MainFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    public static MainFragment newInstance(String param1, String param2) {
+        MainFragment fragment = new MainFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_main, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button book_btn=(Button)view.findViewById(R.id.book_a_doctor_btn);
+        book_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction fragmentTransaction=getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.place_holder,new BookFragment());
+                fragmentTransaction.commit();
+
+            }
+        });
+
+    }
+}
